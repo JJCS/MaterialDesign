06813cae693726b5e91fcee260177476e213fd44
JJCS <jose.jesus.castro.s@gmail.com>
2015-07-13 19:13:23 -0400

    ReadyList Vesion 1.1 navigation draw, list, custom pie

--- app/src/main/java/androidhive/info/materialdesign/activity/HomeFragment.java
+++ app/src/main/java/androidhive/info/materialdesign/activity/HomeFragment.java
@@ -9,9 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidhive.info.materialdesign.R;
-
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class HomeFragment extends Fragment {
+    PieChart pieChart;
+    LinearLayout linearLayout;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,8 +28,28 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+
+    public void onActivityCreated (Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        //try 10/07/2015
+
+        int[] data={6,5,8,4,7,6};
+        int[] color={Color.RED,Color.BLUE,Color.CYAN,Color.GREEN,Color.MAGENTA, Color.GREEN};
+        linearLayout=(LinearLayout)getView().findViewById(R.id.linearLayout);
+        linearLayout.addView(new PieChart(getActivity(), 6, data, color));
+
+        //end try
+
 
     }
+
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
